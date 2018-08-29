0203e6f05b179d5d34e121ae3a35c2db3231b73a
jackrwoods <jackrwoods@gmail.com>
2018-08-29 14:08:07 -0700

    Fixed ddb duplicate data objects issue.

--- controllers/carbon.js
+++ controllers/carbon.js
@@ -12,7 +12,6 @@ router.use(function timeLog(req,res,next){
 
 // Download User Data
 router.get('/download/:UserID', function(req, res) {
-	console.log(req.params.UserID)
 	db.getUser(req.params.UserID).then(function(data) {
 		res.status(200).send(data.data)
 	}).catch((rej) => {
@@ -26,14 +25,8 @@ router.post('/upload', function (req, res) {
 	let usr = req.body
 	usr.onid = usr.UserID
 	delete usr['UserID']
-	console.log(usr)
-
-	db.updateUser(usr).then(function(data) {
-		res.status(200).send('I think it worked.')
-	}).catch((rej) => {
-		res.status(404).send('Well, that didn\'t work.')
-		console.log(rej)
-	})
+	db.updateUser(usr)
+	res.status(200).send('SCV good to go, sir.')
 })
 
 module.exports = router;
--- ddb.js
+++ ddb.js
@@ -13,6 +13,7 @@ exports.initialize = function() {
     state.ddb = new AWS.DynamoDB.DocumentClient()
 }
 
+// This function querys the DynamoDB for the specified user's data.
 exports.getUser = function(onid) {
 	// AWS SDK DDB Query Parameters - https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#query-property
   const params = {
@@ -28,22 +29,62 @@ exports.getUser = function(onid) {
   // Using a promise allows for promise chains.
   return new Promise((resolve, reject) => {
     state.ddb.query(params, function (err, data) {
-      if (err) { return reject(err) }
+      if (err || data.Items.length == 0) { return reject(err) }
       resolve(data.Items[0])
     })
   })
 }
 
-exports.updateUser = function(usr) {
-	// AWS SDK DDB Query Parameters - https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_PutItem.html
+// Uploads a user to the db.
+var putUser = function(usr) {
+  // AWS SDK DDB Query Parameters - https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_PutItem.html
   const params = {
-		'TableName': 'users',
+    'TableName': 'users',
     'Item': usr,
-	}
+  }
+
   return new Promise((resolve, reject) => {
     state.ddb.put(params, function (err, data) {
       if (err) { return reject(err) }
       resolve(data)
     })
   })
+}
+
+// This function compares the user object specified to the corresponding user item
+// in the DynamoDB.
+//      - if the user does not exist, a new user is created with the given data.
+//      - if the user exists, then the function compares the user's data and
+//        updates data objects as necessary. Data objects in the database with
+//        the same date as new data objects will be updated.
+exports.updateUser = function(usr) {
+
+  // Determine if the user exists.
+  this.getUser(usr.onid).then(function(dbUsr) {
+    // User exists.
+
+    // Map data elements to their dates
+    const dataMap = dbUsr.data.map(d => d.date)
+
+    // Iterate over the new data
+    usr.data.forEach(function(data, index, dataArr) {
+      // Search dataMap for the current data's date
+      let i = dataMap.find(data.date)
+      // If the date is found, replace the data object with the most recent
+      if (index > -1) dbUsr.data.splice(i, 1, dataArr[index])
+      // Otherwise, add the data to the list
+      else dbUsr.data.push(dataArr[index])
+
+      // Upload the new user ddb item
+      putUser(dbUsr).catch((rej) => {
+        console.log(rej)
+      })
+    })
+  }).catch((rej) => {
+    // User does not exist.
+    // The user's ddb item will be created
+    putUser(usr).catch((rej) => {
+      console.log(rej)
+    })
+	})
 }
