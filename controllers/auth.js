8cec60135bc04c8cce6332963267f20946e71f7e
jackrwoods <jackrwoods@gmail.com>
2018-09-04 14:15:01 -0700

    Another backup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,9 @@
 const appURL = 'http://ec2-52-39-141-177.us-west-2.compute.amazonaws.com/'
 
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var httpreq = require('httpreq')
 var db = require('../ddb.js')
-const https = require('https');
 db.initialize()
 
 // How CAS login works:
@@ -18,17 +18,21 @@ db.initialize()
 router.get('/login/:returnURI', function (req, res) {
   // Update session
   req.session.returnURI = req.params.returnURI
+  console.log(req.session)
 
   // Redirect user to login url with application url
-	//res.redirect('https://login.oregonstate.edu/idp/profile/cas/login?service=' + appURL)
+	res.redirect('https://login.oregonstate.edu/idp/profile/cas/login?service=' + process.env.CAS_APPLICATION_URL)
 })
 
-// // User logs in successfully and is redirected back to this route
-// router.get('/session', function (req, res) {
-//   //res.send(req.params)
-//   res.send('yes')
-//   // Complete login handshake
-//   //https.get('https://login.oregonstate.edu/idp/profile/cas/serviceValidate?ticket=' + req.params.ticket + '&service=' + appURL)
-// })
+// User logs in successfully and is redirected back to this route
+router.get('/session', function (req, res) {
+
+  // Complete login handshake
+  httpreq.get('http://login.oregonstate.edu/idp/profile/cas/serviceValidate?ticket=' + req.query.ticket + '&service=' + process.env.CAS_APPLICATION_URL, function (err, res) {
+    if (err) return console.log(err)
+    console.log(res.body)
+  })
+
+})
 
 module.exports = router
--- nodeServer.js
+++ nodeServer.js
@@ -9,7 +9,6 @@ var morgan   = require('morgan')
 var port     = process.env.PORT ? process.env.PORT : 3000   // NOTE: Port 80 can be forwarded to 3000 on our ec2 instance
 var db       = require('./db') // Database                   // this command: sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 var bodyParser        = require('body-parser')
-var CASAuthentication = require('cas-authentication')
 var DynamoDBStore     = require('dynamodb-store')
 
 // Allow cross-origin resource sharing
@@ -27,7 +26,7 @@ var storeOptions = {
     'accessKeyId': process.env.AWS_ACCESS_KEY_ID,
     'secretAccessKey': process.env.AWS_SECRET_ACCESS_KEY,
     'region': 'us-west-2',
-    'endpoint': ' https://dynamodb.us-west-2.amazonaws.com',
+    'endpoint': 'http://dynamodb.us-west-2.amazonaws.com',
     'dynamodb_store_debug': true
   },
   'keepExpired': false,
@@ -49,12 +48,40 @@ app.use('/auth',require('./controllers/auth.js'))
 // Carbon-Calculator Application
 app.use('/carbon',require('./controllers/carbon.js'))
 
+//-------------------------------
+var httpreq = require('httpreq')
+db.initialize()
+
+// User initiates login
+router.get('/login/:returnURI', function (req, res) {
+  // Update session
+  req.session.returnURI = req.params.returnURI
+  console.log(req.session)
+
+  // Redirect user to login url with application url
+	res.redirect('https://login.oregonstate.edu/idp/profile/cas/login?service=' + process.env.CAS_APPLICATION_URL)
+})
+
+// User logs in successfully and is redirected back to this route
+router.get('/energy', function (req, res) {
+
+  // Complete login handshake
+  httpreq.get('http://login.oregonstate.edu/idp/profile/cas/serviceValidate?ticket=' + req.query.ticket + '&service=' + process.env.CAS_APPLICATION_URL, function (err, result) {
+    if (err) return console.log(err)
+    console.log(result.body)
+    res.send("done")
+  })
+
+})
+
+//------------------------
+
 // Connect to DB
 db.connect(function (err, connection) {
   if (err) {
     throw err
   }
-  //Start the server
+  // Start the server
   app.listen(port, function () {
     console.log('Server listening on port:', port)
   })
