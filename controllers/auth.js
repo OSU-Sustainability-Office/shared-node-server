ab787c70861a3d2ba53bca40b43f47ab3f1ee8f8
jackrwoods <jackrwoods@gmail.com>
2018-09-18 13:34:01 -0700

    Fixing login route's returnURI bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,8 +15,11 @@ db.initialize()
 // User initiates login
 router.post('/login', function (req, res) {
   // Update session with new returnURI if one is supplied
-  req.session.returnURI = req.body.returnURI.length ? req.body.returnURI : 'http://carbon.campusops.oregonstate.edu/'
-
+  if (req.query.returnURI)
+    req.session.returnURI = req.query.returnURI
+  else
+    req.session.returnURI = 'http://carbon.campusops.oregonstate.edu/'
+  
   // If the user has already logged in, intelligently redirect them back to the source application.
   if (req.session.UserID) res.redirect(req.session.returnURI)
   else {
@@ -28,7 +31,10 @@ router.post('/login', function (req, res) {
 // User initiates login
 router.get('/login', function (req, res) {
   // HTTP GET requests will use URI parameters
-  req.session.returnURI = req.query.returnURI ? req.query.returnURI : 'http://carbon.campusops.oregonstate.edu/'
+  if (req.query.returnURI)
+    req.session.returnURI = req.query.returnURI
+  else
+    req.session.returnURI = 'http://carbon.campusops.oregonstate.edu/'
 
   // If the user has already logged in, intelligently redirect them back to the source application.
   if (req.session.UserID) res.redirect(req.session.returnURI)
