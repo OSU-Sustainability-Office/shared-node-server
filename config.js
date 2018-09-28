2821eb2b0fd2fc15761ee8758794075f94fc116a
Brogan Miner <BroganMiner22@gmail.com>
2018-09-28 11:48:45 -0700

    travis update

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'db': {
-    'mapData': 'mongodb://admin:OakCreek@54.186.223.223:27017/mapData',
-    'carbonData': 'mongodb://admin:OakCreek@54.186.223.223:27017/carbonData'
-  }
-}
