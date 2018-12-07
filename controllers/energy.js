38702f7ff67f798f953cb8777bd793c0510dea59
Brogan Miner <BroganMiner22@gmail.com>
2018-12-07 12:15:44 -0800

    Updates for alerts

--- controllers/energy.js
+++ controllers/energy.js
@@ -297,12 +297,24 @@ router.get('/metersbybuilding', function (req, res) {
 })
 
 router.get('/meterPoints', function (req, res) {
-
+  if (req.queryInt('id')) {
+    db.query('SELECT device_type FROM meters WHERE id = ?', [req.queryInt('id')]).then(r => {
+      fs.readFile('./data/meterdefinitions/' + r[0].device_type + '.json', (err, data) => {
+        if (err) {
+          res.status(400).send('400: ' + err.message)
+        } else {
+          res.send(JSON.stringify(Object.values(JSON.parse(data))))
+        }
+      })
+    })
+  } else {
+    res.status(400).send('400: NO ID')
+  }
 })
 
 router.get('/alerts', function (req, res) {
   if (req.session.user && req.session.user.id) {
-    db.query('SELECT Q2.point AS point, Q2.threshold AS threshold, Q2.id AS id, Q2.meter_name AS meter_name, meter_groups.name AS building_name FROM (SELECT Q1.point AS point, Q1.threshold AS threshold, Q1.name AS meter_name, Q1.id AS id, meter_group_relation.group_id as group_id  FROM (SELECT alerts.point AS point, alerts.threshold AS threshold, meters.id AS meter_id, alerts.id AS id, meters.name AS name FROM alerts LEFT JOIN meters ON alerts.meter_id = meters.id WHERE alerts.user_id = ?) AS Q1 LEFT JOIN meter_group_relation ON Q1.meter_id = meter_group_relation.meter_id) AS Q2 LEFT JOIN meter_groups ON Q2.group_id = meter_groups.id', [req.session.user.id]).then(r => {
+    db.query('SELECT Q2.meter_id AS meter_id, Q2.point AS point, Q2.threshold AS threshold, Q2.id AS id, Q2.meter_name AS meter_name, meter_groups.name AS building_name FROM (SELECT Q1.meter_id AS meter_id, Q1.point AS point, Q1.threshold AS threshold, Q1.name AS meter_name, Q1.id AS id, meter_group_relation.group_id as group_id  FROM (SELECT alerts.point AS point, alerts.threshold AS threshold, meters.id AS meter_id, alerts.id AS id, meters.name AS name FROM alerts LEFT JOIN meters ON alerts.meter_id = meters.id WHERE alerts.user_id = ?) AS Q1 LEFT JOIN meter_group_relation ON Q1.meter_id = meter_group_relation.meter_id) AS Q2 LEFT JOIN meter_groups ON Q2.group_id = meter_groups.id', [req.session.user.id]).then(r => {
       res.send(JSON.stringify(r))
     }).catch(e => {
       res.status(400).send('400: ' + e.message)
