b865070a6ff3fcec84c3f5ce17a7ee4ce164e6d0
Brogan Miner <BroganMiner22@gmail.com>
2019-03-26 19:42:10 -0700

    sustainability map routes

--- controllers/map.js
+++ controllers/map.js
@@ -3,7 +3,7 @@
  * @Date:   2019-03-11T13:57:19-07:00
  * @Email:  brogan.miner@oregonstate.edu
  * @Last modified by:   Brogan
- * @Last modified time: 2019-03-20T15:30:31-07:00
+ * @Last modified time: 2019-03-26T12:01:53-07:00
  */
 
 require('dotenv').config()
@@ -14,7 +14,7 @@ const router = express.Router()
 const db = require('../ddb.js')
 db.initialize()
 
-router.get('/buildings', async function (req, res) {
+router.get('/buildings', async (req, res) => {
   try {
     const token = await APIToken()
     const responseData = await axios('https://api.oregonstate.edu/v1/locations?type=building&page[size]=10000', { method: 'get', headers: { Authorization: 'Bearer ' + token } })
@@ -24,8 +24,15 @@ router.get('/buildings', async function (req, res) {
   }
 })
 
-router.get('/features', function (req, res) {
-
+router.get('/features', async (req, res) => {
+  try {
+    let features = await db.query('sus_map').scan({
+      'Select': 'ALL_ATTRIBUTES'
+    })
+    res.send(features.Items)
+  } catch (err) {
+    res.satus(400).send(err.message)
+  }
 })
 
 module.exports = router
--- ddb.js
+++ ddb.js
@@ -2,8 +2,8 @@
  * @Author: Jack Woods <jackrwoods>
  * @Date:   2018-12-14T13:18:19-08:00
  * @Filename: ddb.js
- * @Last modified by:   Jack Woods
- * @Last modified time: 2019-01-15T13:28:08-08:00
+ * @Last modified by:   Brogan
+ * @Last modified time: 2019-03-26T11:43:00-07:00
  * @Copyright: 2018 Oregon State University
  */
 
@@ -13,6 +13,65 @@
 var AWS = require('aws-sdk')
 AWS.config.update({region: 'us-west-2'})
 
+class DDBQuery {
+  constructor (ddb, table) {
+    this.ddb = ddb
+    this.table = table
+  }
+
+  select (params) {
+    params.TableName = this.table
+
+    return new Promise((resolve, reject) => {
+      this.ddb.query(params, (err, data) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(data)
+      })
+    })
+  }
+
+  scan (params) {
+    params.TableName = this.table
+
+    return new Promise((resolve, reject) => {
+      this.ddb.scan(params, (err, data) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(data)
+      })
+    })
+  }
+
+  update (params) {
+    params.TableName = this.table
+
+    return new Promise((resolve, reject) => {
+      this.ddb.update(params, (err, data) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(data)
+      })
+    })
+  }
+
+  put (params) {
+    params.TableName = this.table
+
+    return new Promise((resolve, reject) => {
+      this.ddb.put(params, (err, data) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(data)
+      })
+    })
+  }
+}
+
 var state = {
   ddb: null
 }
@@ -21,6 +80,10 @@ exports.initialize = function () {
   if (!state.ddb) {
     state.ddb = new AWS.DynamoDB.DocumentClient()
   }
+}
+
+exports.query = function (table) {
+  return new DDBQuery(state.ddb, table)
 }
 
 // This function querys the DynamoDB for the specified user's data.
